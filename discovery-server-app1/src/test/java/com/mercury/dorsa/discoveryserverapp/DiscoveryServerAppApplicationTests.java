9d94de4e1e7809e788d67389a2116b5e094d4b20
srikanth-tiyyagura <56611111+srikanth-tiyyagura@users.noreply.github.com>
2020-10-15 14:27:18 -0400

    Initial Release Version

---
+++ discovery-server-app1/src/test/java/com/mercury/dorsa/discoveryserverapp/DiscoveryServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercury.dorsa.discoveryserverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
